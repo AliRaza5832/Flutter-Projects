3b3bd58379116493e03c2111f7b8fe49c86f4037
AliRaza5832 <141730882+AliRaza5832@users.noreply.github.com>
2023-08-08 16:07:58 +0500

    Create main.dart
    calculator code

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:my_project/home_screen.dart';
+import 'package:my_project/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
